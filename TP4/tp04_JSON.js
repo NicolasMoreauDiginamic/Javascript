b99f67e3bb886ce9a535e2ffec6d0e60667dc6ee
Nicolas Moreau <nicolas.moreau93@orange.fr>
2022-01-19 10:28:46 +0100

    2eme modif TP4

--- TP4/tp04_JSON.js
+++ TP4/tp04_JSON.js
@@ -8,18 +8,21 @@ let societe = {
         {"nom" : "Larry Page","dateNaissance" : "26/03/1973", "villeNaissance" : "East Lansing, Michigan"},
         {"nom" : "Sergey Brin","dateNaissance" : "21/08/1973", "villeNaissance" : "Moscou, Union Soviétique"}
     ],
-    "chiffreAffaire" : [
-        {"annee" : 2008, "valeur" : 16.49},
-        {"annee" : 2012, "valeur" : 37.97},
-        {"annee" : 2016, "valeur" : 89.46},
-        {"annee" : 2018, "valeur" : 136.2}
-
-    ]
+    "chiffreAffaire" : {
+        "unite" : "Millard de $",
+        "valeurs" : [
+            {"annee" : 2008, "valeur" : 16.49},
+            {"annee" : 2012, "valeur" : 37.97},
+            {"annee" : 2016, "valeur" : 89.46},
+            {"annee" : 2018, "valeur" : 136.2}
+    
+        ]
+    }
 };
 
 for(let i = 0; i<societe.fondateur.length; i++){
     console.log(societe.fondateur[i].nom);
 }
-for(let j = 0; j<societe.chiffreAffaire.length; j++){
-    console.log(societe.chiffreAffaire[j].valeur);
+for(let j = 0; j<societe.chiffreAffaire.valeurs.length; j++){
+    console.log(societe.chiffreAffaire.valeurs[j].valeur + " " + societe.chiffreAffaire.unite);
 }
